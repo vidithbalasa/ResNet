5289df1a05c94b6ef0bfe94f524b5c973a62c97b
Vidith Balasa <vidithbalasa@Vidiths-MacBook-Pro.local>
2022-07-30 01:50:05 -0700

    removed validation set

--- train.py
+++ train.py
@@ -7,36 +7,30 @@ from tqdm import tqdm
 from datetime import datetime
 from data_loader import get_CIFAR_data
 
-def train(model: nn.Module, epochs: int=1, save_results: bool=False) -> None:
+def train(model: nn.Module, epochs: int=1, save_name: str='') -> None:
     '''
     Train the model.
     '''
-    trainloader, validloader, testloader, classes = get_CIFAR_data()
+    trainloader, testloader, classes = get_CIFAR_data()
     loss_fn = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=Params.LEARNING_RATE)
-    curr_time = datetime.now().strftime("%m-%d-%Y-%H:%M")
-    res_file = f'results_{curr_time}.csv' if save_results else None
-    if save_results:
-        with open(res_file, 'w') as f:
-            f.write(f'epoch,train_loss,valid_loss,valid_accuracy\n')
+    if save_name:
+        with open(f'{save_name}.csv', 'w') as f:
+            f.write(f'epoch,train_loss,train_accuracy,test_loss,test_accuracy\n')
     
-    for epoch_idx in range(1, epochs+1):
+    for epoch_idx in range(epochs):
         model.train(True)
-        train_loss = train_one_epoch(model, trainloader, optimizer, loss_fn, epoch_idx)
+        train_loss, train_accuracy = train_one_epoch(model, trainloader, optimizer, loss_fn, epoch_idx)
         model.eval()
-        val_accuracy, val_loss = evaluate_model(model, validloader, loss_fn)
-        print(f'\tTrain Loss: {train_loss:.2f} - Valid Loss: {val_loss:.2f} - Valid Accuracy: {val_accuracy*100:.2f}%')
-        if save_results:
-            with open(res_file, 'a') as f:
-                f.write(f'{epoch_idx},{train_loss:.2f},{val_loss:.2f},{val_accuracy*100:.2f}%\n')
+        test_loss, test_accuracy = evaluate_model(model, testloader, loss_fn)
+        print(f'\tTrain Loss: {train_loss:.2f} - Test Loss: {test_loss:.2f} - Test Accuracy: {test_accuracy*100:.2f}%')
+        if save_name:
+            with open(f'{save_name}.csv', 'a') as f:
+                f.write(f'{epoch_idx},{train_loss:.2f},{train_accuracy:.2f}%,{test_loss:.2f},{test_accuracy*100:.2f}%\n')
 
     # save model
-    torch.save(model.state_dict(), f'model_{curr_time}.pt')
-    
-    # get test accuracy
-    model.eval()
-    test_accuracy, test_loss = evaluate_model(model, testloader, loss_fn)
-    print(f'Test loss: {test_loss:.2f} - Test accuracy: {test_accuracy*100:.2f}%')
+    torch.save(model.state_dict(), f'{save_name}.pt')
+    print(f'Model saved to {save_name}.pt')
 
 
 def train_one_epoch(
@@ -45,11 +39,12 @@ def train_one_epoch(
     optimizer: torch.optim, 
     loss_fn: nn.modules.loss, 
     epoch_idx: int,
-) -> float:
+) -> float and float:
         '''
         Train a single epoch.
         '''
         running_loss = 0
+        correct = 0
 
         # tqdm loader with title 'epoch {epoch_idx}'
         for batch_idx, (images, labels) in tqdm(enumerate(trainloader), desc=f'Epoch {epoch_idx}'):
@@ -58,6 +53,9 @@ def train_one_epoch(
             optimizer.zero_grad()
             # get model predictions
             outputs = model(images)
+            # check if the model is correct
+            _, predicted = torch.max(outputs.data, 1)
+            correct += (predicted == labels).sum().item()
             # compute loss & gradients
             loss = loss_fn(outputs, labels)
             loss.backward()
@@ -65,7 +63,7 @@ def train_one_epoch(
             optimizer.step()
 
             running_loss += loss.item()
-        return running_loss / len(trainloader)
+        return running_loss / len(trainloader), correct / len(trainloader)
 
 def evaluate_model(model: nn.Module, validloader: DataLoader, loss_fn: nn.modules.loss) -> float:
     '''
@@ -87,4 +85,4 @@ def evaluate_model(model: nn.Module, validloader: DataLoader, loss_fn: nn.module
             # compute loss
             loss = loss_fn(outputs, labels)
             running_loss += loss.item()
-    return correct / total, running_loss / len(validloader)
+    return running_loss / len(validloader), correct / total
